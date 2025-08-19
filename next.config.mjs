e85481a3148a682468fc52526f727e998c608305
Sakibhaqie <imdadulhaque72@gmail.com>
2025-08-19 17:52:43 +0530

    added

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-  basePath: "/gafah",
-  assetPrefix: "/gafah/",
-  experimental: {
-    instrumentationHook: false,
-  },
-  // Let Nginx handle the routing instead
-};
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+  images: { unoptimized: true },
+}
 
 export default nextConfig;
